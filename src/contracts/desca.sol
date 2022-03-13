5ce8e869b052c02137e26d6f33d7f32f13ed4390
James <jamestackett1123@gmail.com>
2022-03-13 15:42:39 -0500

    Added basic functionality Created data storage for contract and set up inital parameters.

--- src/contracts/desca.sol
+++ src/contracts/desca.sol
@@ -2,8 +2,46 @@
 
 pragma solidity ^0.8.0;
 
-contract desca {
-    constructor() {
-        
+import "@openzeppelin/contracts/access/AccessControl.sol";
+
+contract DeSCA is AccessControl {
+    //roles
+    bytes32 public constant DATA_SENDER_ROLE = keccak256("DATA_SENDER_ROLE");
+    bytes32 public constant DRONE_CONTROLLER_ROLE = keccak256("DRONE_CONTROLLER_ROLE");
+    bytes32 public constant CONTRACT_EDITOR_ROLE = keccak256("CONTRACT_EDITOR_ROLE");
+
+    //node struct
+    struct node {
+        address nodeAddress; // blockchain address
+        int256 nodeData; // received data
+        uint256 noDataTimer; // counts cycles since data received
+        bool dataReceived; // has send data this cycle
+    }
+
+    //datastore
+    uint256 public totalNodes;  // total nodes on network
+    uint256 public functionalNodes; // nodes not timed out
+    uint256 public targetPercentage; // percentage of nodes above threshold
+    uint256 public nodeTimeout; // number of times node can not send data before being skipped
+    bool public flightflag; // flag for sending drone out
+    node[] public nodeList; // holds all nodes
+    
+
+    constructor(uint256 _targetpercent, uint256 _timeout) {
+        _setupRole(DEFAULT_ADMIN_ROLE, msg.sender);
+        totalNodes = 0;
+        targetPercentage = _targetpercent;
+        nodeTimeout = _timeout;
+        flightflag = false;
+    }
+
+    function addnode(address _nodeaddress) public {
+        require(hasRole(DEFAULT_ADMIN_ROLE, msg.sender));
+        if(functionalNodes < totalNodes) {
+            nodeList.push(node({nodeAddress: _nodeaddress, nodeData: 0, noDataTimer: 0, dataReceived: false}));
+            grantRole(DATA_SENDER_ROLE, _nodeaddress);
+            functionalNodes += 1;
+            totalNodes += 1;
+        }
     }
 }
