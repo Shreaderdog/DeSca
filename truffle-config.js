db299bccde9cc9a849afd7e32fccfb1ca150c5ac
James <jamestackett1123@gmail.com>
2022-04-12 11:57:46 -0500

    Set up React Project

--- truffle-config.js
+++ truffle-config.js
@@ -10,7 +10,7 @@ module.exports = {
     },
   },
   contracts_directory: "./src/contracts/",
-  contracts_build_directory: "./src/abis/",
+  contracts_build_directory: "./src/client/contracts/",
   compilers: {
     solc: {
       version: "pragma",
