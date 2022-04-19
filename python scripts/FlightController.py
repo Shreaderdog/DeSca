a2c770ced820fa23c9e40a011dfde26d203bc6c3
James <jamestackett1123@gmail.com>
2022-04-19 11:33:07 -0500

    Added flight script to the flight controller

--- python scripts/FlightController.py
+++ python scripts/FlightController.py
@@ -1,6 +1,8 @@
 import time
 import socket
 import json
+import RPi.GPIO as GPIO
+import time
 from sensorToBlockchain import BUFFER_SIZE
 from web3 import Web3
 
@@ -28,4 +30,36 @@ while (true):
 
 
 def fly():
-    print("we flyin!")
+
+    GPIO.setmode(GPIO.BOARD)
+    GPIO.setwarnings(False)
+    ledPin = 12
+    GPIO.setup(ledPin, GPIO.OUT)
+    for i in range(5):
+        print("drone is going east")
+        GPIO.output(ledPin, GPIO.HIGH)
+        time.sleep(0.5)
+        print("LED turning off.")
+        GPIO.output(ledPin, GPIO.LOW)
+        time.sleep(0.5)
+    for i in range(7):
+        print("drone is going west")
+        GPIO.output(ledPin, GPIO.HIGH)
+        time.sleep(0.5)
+        print("LED turning off.")
+        GPIO.output(ledPin, GPIO.LOW)
+        time.sleep(0.5)
+    for i in range(2):
+        print("drone is going north")
+        GPIO.output(ledPin, GPIO.HIGH)
+        time.sleep(0.5)
+        print("LED turning off.")
+        GPIO.output(ledPin, GPIO.LOW)
+        time.sleep(0.5)
+    for i in range(7):
+        print("drone is going south")
+        GPIO.output(ledPin, GPIO.HIGH)
+        time.sleep(0.5)
+        print("LED turning off.")
+        GPIO.output(ledPin, GPIO.LOW)
+        time.sleep(0.5)
