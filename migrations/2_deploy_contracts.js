80de6dc8dcf725ec62c3e9346bdb1b4c0c7ec105
James <jamestackett1123@gmail.com>
2022-04-20 18:09:35 -0500

    created graph component

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,9 +1,9 @@
 const datacontract = artifacts.require("DeSCA");
 const daocontract = artifacts.require("Dao");
 
-module.exports = async function(deployer, network, accoutns) {
+module.exports = async function(deployer, network, accounts) {
     await deployer.deploy(datacontract, 75, 3);
     const sensorcontract = await datacontract.deployed();
 
-    await deployer.deploy(daocontract, 6, sensorcontract);
+    await deployer.deploy(daocontract, 6, sensorcontract.address);
 }
